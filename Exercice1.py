1e18ee56d0d1f28371395718878f98633113e6bc
Mael-git <mael.mock-gaspalou@isen-ouest.yncrea.fr>
2020-04-08 16:41:41 +0200

    Normalement ca marche

--- Exercice1.py
+++ Exercice1.py
@@ -3,19 +3,24 @@ class Node:
         self.__val = val
         self.__right = right
         self.__left = left
+
     def getVal(self):
         return self.__val
+
     def getRight(self):
         return self.__right
+
     def getLeft(self):
         return self.__left
+
     def setRight(self,node):
         self.__right = node
+
     def setLeft(self,node):
         self.__left = node
 
-"""""
-node1 = Node(3,None,None)
-node2 = Node(4,None,None)
-node3 = Node(1,node1,node2)
-print(node3.getLeft().getVal())"""
+
+node1 = Node(2,None,None)
+node2 = Node(8,None,None)
+node3 = Node(5,node1,node2)
+print(node3.getLeft().getVal())
