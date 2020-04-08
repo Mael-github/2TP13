45404a1d0e319c2284a0600a48f7e4585ef6d49f
Mael-git <mael.mock-gaspalou@isen-ouest.yncrea.fr>
2020-04-08 16:39:43 +0200

    Initial commit

---
+++ Exercice1.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,val,left,right):
+        self.__val = val
+        self.__right = right
+        self.__left = left
+    def getVal(self):
+        return self.__val
+    def getRight(self):
+        return self.__right
+    def getLeft(self):
+        return self.__left
+    def setRight(self,node):
+        self.__right = node
+    def setLeft(self,node):
+        self.__left = node
+
+"""""
+node1 = Node(3,None,None)
+node2 = Node(4,None,None)
+node3 = Node(1,node1,node2)
+print(node3.getLeft().getVal())"""
